474e4820c947638aa86da61802b4441d6e534d50
Brepe <bre.perdo@gmail.com>
2018-09-09 13:59:31 -0300

    Criação de funcionário entity e classe pro bd.

---
+++ AppService.Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using AppService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppService.Domain.Repositories
+{
+    public interface IBaseRepository<T> where T: BaseEntity
+    {
+        Task<T> GetItemAsync(Guid id);
+        void AddItemAsync(T item);
+        Task<bool> DeleteItemAsync(Guid id);
+        Task<T> UdateItemAsync(Guid id, T item);
+        Task<IEnumerable<T>> GetAllItemsAsync();
+    }
+}
