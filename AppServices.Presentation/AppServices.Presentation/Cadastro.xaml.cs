49a52d11fe20a275a242fc6e9ff884be8980f5f3
Brepe <bre.perdo@gmail.com>
2018-10-20 17:44:23 -0300

    Templates das páginas de login e de cadastro

--- AppServices.Presentation/AppServices.Presentation/Cadastro.xaml.cs
+++ AppServices.Presentation/AppServices.Presentation/Cadastro.xaml.cs
@@ -13,9 +13,37 @@ namespace AppServices.Presentation
 	public partial class Cadastro : ContentPage
 	{
 		public Cadastro ()
+
 		{
-			InitializeComponent ();
+
+            InitializeComponent();
+            myPicker.Items.Add("Região 1");
+            myPicker.Items.Add("Região 2");
+            myPicker.Items.Add("Região 3");
+            myPicker.Items.Add("Região 4");
+
             //await Navigation.PopAsync();
         }
-	}
+        async void OnButtonClicked3(object sender, EventArgs args)
+        {
+
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            double value = args.NewValue;
+            Resp1.Text = String.Format("Preço por hora (garçom): {0}", value);
+        }
+        void OnSliderValueChanged2(object sender, ValueChangedEventArgs args)
+        {
+            double value = args.NewValue;
+            Resp2.Text = String.Format("Preço por hora (fritadeira): {0}", value);
+        }
+        void OnSliderValueChanged3(object sender, ValueChangedEventArgs args)
+        {
+            double value = args.NewValue;
+            Resp3.Text = String.Format("Preço por hora (copeira): {0}", value);
+        }
+    }
 }
